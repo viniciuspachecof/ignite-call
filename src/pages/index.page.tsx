6fc840db43de6ca46aa5a41efd2082f7c66bda3b
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-07-23 22:38:50 -0300

    Primeiro commit (Preparando setup do projeto)

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,9 @@
+import { Heading, Text } from '@ignite-ui/react';
+
+export default function Home() {
+  return (
+    <>
+      <Heading as="h1">Teste</Heading>
+    </>
+  );
+}
---
+++ src/styles/globals.ts
@@ -0,0 +1,20 @@
+import { globalCss } from '@ignite-ui/react';
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    '-webkit-font-smoothing': 'antialiased',
+    backgroundColor: '$gray900',
+    color: '$gray100',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+  },
+});
