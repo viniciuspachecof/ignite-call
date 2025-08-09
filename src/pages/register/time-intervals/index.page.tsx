4f4b7556dbbec612ae907aed5011186c5a6fa77c
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-08-08 22:56:13 -0300

    input e output do zod, rotas autenticadas e persistindo intervalos

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,6 +7,9 @@ import z from 'zod';
 import { getWeekDays } from '@/src/utils/get-week-days';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import { convertTimeStringToMinutes } from '../../../utils/convert-time-string-to-minutes';
+import { api } from '@/src/lib/axios';
+
 const timeIntervalsFormSchema = z.object({
   intervals: z
     .array(
@@ -21,10 +24,29 @@ const timeIntervalsFormSchema = z.object({
     .transform((intervals) => intervals.filter((interval) => interval.enabled))
     .refine((intervals) => intervals.length > 0, {
       message: 'Você precisa selecionar ao menos um dia da semana!',
-    }),
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        };
+      });
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every((interval) => interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes);
+      },
+      {
+        message: 'O horário de término deve ser pelo menos 1h distante do início.',
+      }
+    ),
 });
 
-type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>;
+// type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>;
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>;
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>;
 
 export default function TimeIntervals() {
   const {
@@ -57,8 +79,8 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals');
 
-  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
-    console.log(data);
+  async function handleSetTimeIntervals(data: TimeIntervalsFormOutput) {
+    await api.post('/user/time-intervals', data);
   }
 
   return (
