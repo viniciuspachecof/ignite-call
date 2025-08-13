77477b4cbc75f8baa02d0f71bf97ffb54f3a4169
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-08-12 22:39:01 -0300

    Página de agendamento e visual do calendário

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,76 @@
+import { CaretLeftIcon, CaretRightIcon } from '@phosphor-icons/react';
+import { CalendarActions, CalendarBody, CalendarContainer, CalendarDay, CalendarHeader, CalendarTitle } from './styles';
+import { getWeekDays } from '@/src/utils/get-week-days';
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true });
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Agosto <span>2025</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeftIcon />
+          </button>
+          <button>
+            <CaretRightIcon />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  );
+}
---
+++ src/pages/schedule/[username]/SheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,3 @@
+export function CalendarStep() {
+  return <h1>calendar step</h1>;
+}
